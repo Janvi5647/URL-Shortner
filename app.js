a866322b12440161b70636542f601de1e8c0d758
Janvi Kanani <janvikanani005@gmail.com>
2025-05-05 22:43:45 +0530

    commit

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-import { createServer } from "http";
-import { readFile, writeFile } from "fs/promises";
+
+import {readFile,writeFile} from "fs/promises";
 import path from "path";
 import crypto from "crypto";
+import express from "express";
+
+const app = express();
 
 const DATA_FILE = path.join("data", "links.json");
 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
 const serverFile = async (res, filePath, contentType) => {
   try {
-    var data = await readFile(filePath,'utf-8');
+    var data = await readFile(filePath, "utf-8");
     res.writeHead(200, { "Content-Type": contentType });
     res.end(data);
   } catch (error) {
@@ -37,58 +43,69 @@ const saveLinks = async (links) => {
   }
 };
 
-const server = createServer(async (req, res) => {
-  if (req.method === "GET") {
-    if (req.url === "/") {
-      serverFile(res, path.join("public", "index.html"), "text/html");
-    } else if (req.url === "/style.css") {
-       serverFile(res, path.join("public", "style.css"), "text/css");
-    } else if (req.url == "/links") {
-      const links = await LoadLinks();
-      res.writeHead(200, { "Content-Type": "application/json" });
-      return res.end(JSON.stringify(links));
-    }else{
-        const links = await LoadLinks();
-        const shortCode = req.url.slice(1);
-        if(links[shortCode]){
-            res.writeHead(302, { Location: links[shortCode] });
-            return res.end();
-        }
-        res.writeHead(400, { "Content-Type": "text/plain" });
-        res.end("Page not found" );
-    }
-  }
-
-  if (req.method === "POST" && req.url === "/shorten") {
+app.get("/", async (req, res) => {
+  try {
+    const file = await readFile(
+      path.join("views", "index.html"),
+      "utf-8"
+    );
     const links = await LoadLinks();
 
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
-    req.on("end", async () => {
-      const { url, shortCode } = JSON.parse(body);
-      if (!url) {
-        res.writeHead(400, { "Content-Type": "text/plain" });
-        res.end("URL is required" );
-      }
-
-      const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
-      if (links[finalShortCode]) {
-        res.writeHead(400, { "Content-Type": "text/plain" });
-        return res.end("Short code already exists" );
-      }
-      links[finalShortCode] = url;
-
-      await saveLinks(links);
-
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ shortCode: finalShortCode }));
-    });
+    const content = file.toString().replaceAll(
+      "{{shortened_urls}}",
+      Object.entries(links)
+        .map(
+          ([shortCode, url]) =>
+            `<li><a href="/${shortCode}" target="_blank">${req.host}/${shortCode}</a> -> ${url}</li>`
+        )
+        .join("")
+    );
+    return res.send(content);
+  } catch (error) {
+    return res.status(500).send("Internal Server Error");
   }
 });
 
+app.post("/", async (req, res) => {
+  try {
+    const { url, shortCode } = req.body;
+    const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
+
+    const links = await LoadLinks();
+
+    if (links[finalShortCode]) {
+      return res.status(400).send("Short code already exists");
+    }
+
+    links[finalShortCode] = url;
+    await saveLinks(links);
+
+    return res.redirect("/");
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/:shortCode", async (req, res) => {
+  try {
+    const { shortCode } = req.params;
+    const links = await LoadLinks();
+
+    if(!links[shortCode]) {
+      return res.status(404).send("Short code not found");
+    }
+
+    return res.redirect(links[shortCode]);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
+
+
 const port = 9000;
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
